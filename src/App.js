d48b2496645afb53a6ff6ed800f1c4fb9d97ef04
HenrikHao <73416544+HenrikHao@users.noreply.github.com>
2025-02-22 19:04:03 +1100

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Resume from './Resume';
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <Resume />
+    </div>
+  );
+}
+
+
+export default App;
