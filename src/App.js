57b6d9a7054bd5c293d35296af2ea1be663900b7
HenrikHao <73416544+HenrikHao@users.noreply.github.com>
2025-02-26 15:22:27 +1100

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
-import Resume from './Resume';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <Resume />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add more routes as you create the pages */}
+        {/* <Route path="/projects" element={<Projects />} /> */}
+        {/* <Route path="/experience" element={<Experience />} /> */}
+        {/* <Route path="/resume" element={<Resume />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
   );
 }
-
 
 export default App;
