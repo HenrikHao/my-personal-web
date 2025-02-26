dc68ec592626673df36a8b8ad095c92c61ae3546
HenrikHao <73416544+HenrikHao@users.noreply.github.com>
2025-02-26 17:42:05 +1100

    reset project env

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "load_balancer_ip" {
-  value       = google_compute_global_forwarding_rule.website_forwarding_rule.ip_address
-  description = "IP address of the load balancer - use this to create an A record in Namesilo"
-}
-
-output "bucket_url" {
-  value       = google_storage_bucket.website_bucket.url
-  description = "The base URL of the bucket"
-}
