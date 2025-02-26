a4167ecf0032b26afe1756f46d97708faae78e92
HenrikHao <73416544+HenrikHao@users.noreply.github.com>
2025-02-26 17:50:59 +1100

    keep terraform script

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type        = string
+  description = "personal-web-451923"
+}
+
+variable "region" {
+  type        = string
+  description = "Google Cloud Region"
+  default     = "us-central1"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "me.henrikhao.com"
+}
