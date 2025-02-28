af05e2b6c39df3784396f9e1fadf46e157323fdf
HenrikHao <73416544+HenrikHao@users.noreply.github.com>
2025-02-28 15:04:27 +1100

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const navLinks = [
@@ -21,15 +21,38 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+
+    // Use state addBlur start with false and set to true when the user scrolls down 100px
+    const [addBlur, setAddBlur] = useState(false);
+
+    // if the user scrolls down 100px, set addBlur to true
+    const handleScroll = () => {
+        if (window.scrollY >= 100) {
+            setAddBlur(true);
+        } else {
+            setAddBlur(false);
+        }
+    };
+
+    // useEffect is used to add an event listener to the window when the component mounts
+    // the last [] is for dependencies, empty means it will only run once
+    // handleScroll is the function that will be called when the user scrolls
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         // <nav> typically tells the browser this is a navigation bar
-        // sticky top-0 z-50 bg-gray-800 shadow-md is a class that makes the navbar sticky to the top of the page, has a gray background, and a shadow
+        // sticky top-0 z-50 bg-transparent shadow-none is a class that makes the navbar transparent and no shadow
         // max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 is a class that makes the navbar the full width of the page, with padding on the left and right
         // flex items-center justify-between h-16 is a class that makes the navbar items centered and the height of the navbar
         //Outer element for positioning and background
         //Middle element for width constraints and padding
         //Inner element for content layout
-        <nav class="sticky top-0 z-50 bg-gray-800 shadow-md">
+        <nav class={`fixed top-0 w-full z-50 ${addBlur ? 'backdrop-blur-md bg-gray-800/50' : 'bg-transparent'} transition-all duration-300`}>
             <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex items-center justify-between h-24">
                     <div class="flex-shrink-0">
